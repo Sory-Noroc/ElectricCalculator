b609a99b688697a9c0d40010b58f03bab2b12ad2
Sory02 <sorinnoroc1@gmail.com>
2023-03-08 20:52:47 +0200

    Initial commit

---
+++ app/src/main/java/com/sorychan/elecalc/viewmodels/DevicesViewModel.kt
@@ -0,0 +1,13 @@
+package com.sorychan.elecalc.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DevicesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
