0b62a9150276e983caacbba1c10220690446bf46
Sory02 <sorinnoroc1@gmail.com>
2023-03-29 17:28:24 +0300

    Testing cost formats

---
+++ app/src/test/java/com/sorychan/elecalc/data/MeasurementTest.kt
@@ -0,0 +1,20 @@
+package com.sorychan.elecalc.data
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class MeasurementTest {
+
+    @Test
+    fun test_format_cost() {
+        val testDevice = Device(100L, 2L, 5L)
+        assertEquals(testDevice.formatCost(), "0")
+        assertEquals(testDevice.apply { cost = 999 }.formatCost(), "999")
+        assertEquals(testDevice.apply { cost = 1001 }.formatCost(), "1K")
+        assertEquals(testDevice.apply { cost = 10000 }.formatCost(), "10K")
+        assertEquals(testDevice.apply { cost = 1000000 }.formatCost(), "1M")
+        assertEquals(testDevice.apply { cost = 1010017 }.formatCost(), "1M")
+        assertEquals(testDevice.apply { cost = 1000000000 }.formatCost(), "1B")
+        assertEquals(testDevice.apply { cost = 999999999 }.formatCost(), "999M")
+    }
+}
